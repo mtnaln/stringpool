c336787391d1462992bb9ab8a1c1ba93f2e2bba3
mtnaln <metinalniacik@gmail.com>
2018-08-25 17:23:24 +0300

    Örnek tamamlandı.

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * Metin Alnıaçık, 25.08.2018
+ * String pool
+ *
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String name = "metin";
+        String name2 = "metin";
+        String name3 = new String("metin");
+        String name4 = new String("metin");
+
+        System.out.println("name ve name2 -- 'name' ve 'name2' string poolda olduğu için true mesajı dönecektir. ==> " + (name == name2));
+        System.out.println("name ve name3 -- 'name' string poolda 'name3' ise string poolda değildir. Bundan dolayı sonuç false olacaktır. ==> " + (name == name3));
+        System.out.println("name3 ve name4 -- 'name3' ve 'name4' her ikisi de string poolda değildir. Bundan dolayı sonuç false olacaktır. ==> " + (name3 == name4));
+    }
+}
